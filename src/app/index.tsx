e1464bc7eb6919e46023d77fb195286fb79d2054
Raul Villanueva III <106396891+Villanueva0909@users.noreply.github.com>
2026-05-21 15:27:33 +0800

    Refactor layout components for improved structure; add global styles file

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,16 @@
-import { Text, View, StyleSheet } from "react-native";
+import { View } from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+    <View style={{ 
+      flex: 1, 
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "black"
+      }}>
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+
