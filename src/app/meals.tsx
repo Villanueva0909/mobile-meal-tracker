10535368c1e51247751a059f297efc64ed3b7f30
Raul Villanueva III <106396891+Villanueva0909@users.noreply.github.com>
2026-05-26 11:36:50 +0800

    Refactor layout structure and implement HomeScreen for meal tracking

--- src/app/meals.tsx
+++ src/app/meals.tsx
@@ -1,4 +1,3 @@
-import { Link } from 'expo-router';
 import { ScrollView, Text } from 'react-native';
 import HomeHeader from './components/HomeHeader';
 import { globalStyles } from './styles/global';
@@ -6,10 +5,9 @@ import { globalStyles } from './styles/global';
 export default function HomeScreen() {
   return (
     <ScrollView style={globalStyles.container}>
-      <Text style={globalStyles.title}>Meal Tracker</Text>
+      <Text style={globalStyles.title}>All Meals</Text>
       {/* <Text style={globalStyles.date}>Monday, March 16</Text> */}
       <HomeHeader />
-      <Link href="/meals" style={{ fontSize: 18, color: '#007bff' }}>Go to Meals</Link>
     </ScrollView>
   );
 }
